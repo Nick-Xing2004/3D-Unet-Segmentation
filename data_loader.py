1dca328a310e33c66de7e898ca697876ed403ec6
Nick Xing <yuyang.xing@vanderbilt.edu>
2025-06-12 15:01:38 -0500

    image & mask cropping and padding, mask padding finished

--- data_loader.py
+++ data_loader.py
@@ -3,13 +3,14 @@ import os
 import torch
 from torch.utils.data import Dataset, DataLoader, random_split
 import nibabel as nib
+import torch.nn.functional as F
 
 class HipDataset(Dataset):
     def __init__(self, root_dir):
         self.root_dir=root_dir
         self.sample_dirs = [d for d in os.listdir(root_dir) if os.path.isdir(os.path.join(root_dir, d))]
 
-    def _len_(self):
+    def __len__(self):
         return len(self.sample_dirs)
     
     #data loader loading
@@ -23,10 +24,30 @@ class HipDataset(Dataset):
         mask_path = os.path.join(sample_dir, f"{self.sample_dirs[idx]}_bone_mask-1.nii.gz")
         mask = nib.load(mask_path).get_fdata()
 
+        # Permute to [D, H, W]
+        image = image.transpose(2, 0, 1)  # [H, W, D] → [D, H, W]
+        mask = mask.transpose(2, 0, 1)
+
         #convert to tensors
         image_tensor = torch.from_numpy(image).float().unsqueeze(0) # add channel dimension    #[1, D, H, W]
         mask_tensor = torch.from_numpy(mask).float().unsqueeze(0) # add channel dimension    #[1, D, H, W]
+
+        #resize the mask to match the image size in 'depth' dimension
+        image_depth = image_tensor.shape[1]
+        mask_depth = mask_tensor.shape[1]
+        if image_depth != mask_depth:
+            pad_size = image_depth - mask_depth
+            padding = torch.zeros((1, pad_size, mask_tensor.shape[2], mask_tensor.shape[3]), dtype=mask_tensor.dtype)
+            #padding the mask tensor
+            mask_tensor = torch.cat((mask_tensor, padding), dim=1)
+        
+        #image and mask tensor cropping
+        target_depth = 512      #desired depth for cropping or padding
+        image_tensor = crop_or_pad_depth(image_tensor, target_depth)
+        mask_tensor = crop_or_pad_depth(mask_tensor, target_depth)
+
         return image_tensor, mask_tensor
+
 
 def dataloader(Args):
     """
@@ -55,3 +76,23 @@ def dataloader(Args):
     print(f'validation dataset size: {len(val_dataset)}')
     
     return train_loader, val_loader
+
+
+#helper function for cropping sample images and masks to the same size
+def crop_or_pad_depth(tensor, target_depth):
+    _, D, H, W = tensor.shape
+    if D == target_depth:
+        return tensor
+    
+    elif D > target_depth:
+        start = (D - target_depth) // 2
+        return tensor[:, start:start + target_depth, :, :]
+    
+    else:      # pad the tensor to match the target depth
+        pad_total = target_depth - D
+        pad_front = pad_total // 2
+        pad_back = pad_total - pad_front
+        pad_shape = (0, 0, 0, 0, pad_front, pad_back)
+        return F.pad(tensor, pad_shape, mode='constant', value=0)
+
+        
