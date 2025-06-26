4cd163485c3b5b6ba82499e20350dbfb822aa9e2
Nick Xing <yuyang.xing@vanderbilt.edu>
2025-06-26 18:06:30 -0500

    one more time

--- train.py
+++ train.py
@@ -57,7 +57,7 @@ def train(model, args, device):
         #model parameters saving with avg_val_loss as the criterion
         if avg_val_loss < best_val_loss:
             best_val_loss = avg_val_loss
-            torch.save(model.state_dict(), "best_Unet_3D_Yuyang_6th_version.pth")
+            torch.save(model.state_dict(), "best_Unet_3D_Yuyang_7th_version.pth")
             print(f"Saved new best model✅! At epoch {epoch+1} with avg_val_loss: {avg_val_loss:.4f}")
         
         #recording the training history
